00d5bcce7476c8b5379158758e82e8c2303bf6f8
Hanif <andque12345012@gmail.com>
2025-03-16 01:00:07 +0700

    Change Incorrect config.js at backend

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'mydatabase',
-    host: process.env.DB_HOST || '127.0.0.1',
-    dialect: process.env.DB_DIALECT || 'mysql',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || 'password',
+    database: process.env.DB_NAME || 'bee_seeker_db',
+    host: process.env.DB_HOST || 'postgres_db',
+    dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME_TEST || 'mydatabase_test',
-    host: process.env.DB_HOST || '127.0.0.1',
-    dialect: process.env.DB_DIALECT || 'mysql',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || 'password',
+    database: process.env.DB_NAME_TEST || 'bee_seeker_db_test',
+    host: process.env.DB_HOST || 'postgres_db',
+    dialect: 'postgres',
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    dialect: 'postgres',
   },
-}
+};
